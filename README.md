caf0f16153805f1eb175696a052245737ea3612f
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-05-10 20:22:50 +0200

    ci: add a lot of linting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![CI](https://github.com/unewMe/solvro-frontend/actions/workflows/ci.yaml/badge.svg)](https://github.com/unewMe/solvro-frontend/actions/workflows/ci.yaml)
+
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import styles from '../styles/home.module.css';
+import styles from '../styles/Home.module.css';
 import Hero from '../components/Hero';
 
 const Page = () => {
   return (
     <div className= {styles.background_container}>
-    <div className={styles.background_overlay}></div>
+    <div className={styles.background_overlay} ></div>
       <NavBar />
       <div className={styles.page_container}>
         <Hero />
